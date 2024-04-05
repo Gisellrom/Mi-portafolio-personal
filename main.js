bb4ba4b18cc09c46cd03b7bd95c7dcb3c239c919
Gisellrom <romero.giselln@gmail.com>
2024-04-04 21:20:59 -0600

    cambia color del mensaje

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ let app = document.getElementById('typewriter');
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
- cursor: "<span style='color: #fb4993;'>|</span>",
+ cursor: "<span style='color: #dc8f90;'>|</span>",
 });
  
 typewriter
   .pauseFor(2500)
-  .typeString('<span style="color: #fb4993;">Soy estudiante de programación web y ciencias de la computación</span>')
+  .typeString('<span style="color: #dc8f90;">Soy estudiante de programación web y ciencias de la computación</span>')
   .pauseFor(200)
   .deleteChars(10)
   .start();
